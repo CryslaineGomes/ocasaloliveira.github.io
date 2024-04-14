3df9c538bc83ef1542d4aa87ea0cdb44949f557b
CryslaineGomes <104288350+CryslaineGomes@users.noreply.github.com>
2024-04-13 22:02:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('download-btn').addEventListener('click', function() {
+    // Coloque aqui o código para iniciar o download do livro
+  });
+  
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Coloque aqui o código para processar o formulário de contato
+  });
+  
