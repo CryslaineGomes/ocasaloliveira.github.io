f3a81a5c7f20f0bf70a5698484ffe09f4667d1fd
CryslaineGomes <104288350+CryslaineGomes@users.noreply.github.com>
2024-10-01 22:14:42 -0300

    Atualizar o script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('download-btn').addEventListener('click', function() {
-    // Coloque aqui o código para iniciar o download do livro
-  });
-  
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Coloque aqui o código para processar o formulário de contato
-  });
-  
