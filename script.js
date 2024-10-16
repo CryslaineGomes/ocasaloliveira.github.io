0a12a0659433bc4f1edf2169a267b1b09741d481
CryslaineGomes <104288350+CryslaineGomes@users.noreply.github.com>
2024-10-15 23:06:24 -0300

    Atualizar o script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactButton = document.querySelector(".cta-button");
+    
+    contactButton.addEventListener("click", function() {
+        alert("Entraremos em contato com você em breve!");
+    });
+});
